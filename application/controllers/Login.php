b1fc16d58cbb8573b7e2e6a7fcc280c4860d4d54
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-02 18:48:57 -0300

    Validando formulario de login e testando

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,6 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
 		// Titulo da aba no navegador
@@ -12,9 +19,19 @@ class Login extends CI_Controller
 
 		// Passa um conjunto de variaveis para as views
 		$this->load->vars($dados);
+		
+		// Validando os inputs
+		$this->form_validation->set_rules('login', 'E-mail', 'required');
+		$this->form_validation->set_rules('senha', 'Senha', 'required');
 
-		$this->load->view('templates/header');
-		$this->load->view('pages/login');
-		$this->load->view('templates/footer');
+		if ($this->form_validation->run() == TRUE) {
+			echo '<pre>';
+			print_r($this->input->post());
+			echo '</pre>';
+		} else {
+			$this->load->view('templates/header');
+			$this->load->view('pages/login');
+			$this->load->view('templates/footer');
+		}
 	}
 }
