6c5325ff21050e4975adfab29613ecafb4e544d3
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-02 16:20:35 -0300

    Add verificação de login na view Inicial

---
+++ application/controllers/Home.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		if (!$this->ion_auth->logged_in()) {
+			redirect('login');
+		}
+	}
+
+	public function index()
+	{
+		// Titulo da aba no navegador
+		$dados["titulo"] = "Inicial";
+
+		// Pega os dados do Model
+
+		// Passa um conjunto de variaveis para as views
+		$this->load->vars($dados);
+
+		$this->load->view('templates/header');
+		$this->load->view('pages/index');
+		$this->load->view('templates/footer');
+	}
+}
