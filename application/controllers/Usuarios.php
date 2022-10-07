8c019634653aea5904ff37621876420a8706fac4
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-06 22:14:48 -0300

    Criando e add Verificçao e login em Usuarios

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Verifica se está logado no sistema
+        if (!$this->ion_auth->logged_in()) {
+            setar_msg('msgerro', 'Erro: Você precisa estar logado no sistema.', 'erro');
+            redirect('login');
+        }
+        $this->load->model('ion_auth_model');
+    }
+
+}
