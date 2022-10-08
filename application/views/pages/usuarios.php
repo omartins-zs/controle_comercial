15373ec2c4ca794ddb8ff60822905edb2743e280
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-08 18:49:42 -0300

    Add button de novo usuario e comentarios

--- application/views/pages/usuarios.php
+++ application/views/pages/usuarios.php
@@ -18,48 +18,52 @@
            </div>
            <!-- /.row -->
 
-           <?php
-            // echo '<pre>';
-
-            // print_r($users);
-            // echo '</pre>';
-
-            ?>
-
+           <!-- Button de novo usuario -->
            <div class="row">
-
-               <div class="col-lg-12">
-
-                       <table class="table table-bordered table-striped" id="datatable">
-                           <thead>
-                               <tr>
-                                   <th>#</th>
-                                   <th>Nome</th>
-                                   <th>E-mail</th>
-                                   <th>Ações</th>
-                                   <!-- <th></th> -->
-                               </tr>
-                           </thead>
-                           <tbody>
-                               <?php foreach ($users as $user) : ?>
-                                   <tr>
-                                       <td><?= $user->id ?></td>
-                                       <td><?= $user->username ?></td>
-                                       <td><?= $user->email ?></td>
-                                       <td class="text-right">
-                                           <a href="<?php base_url() ?>usuarios/editar/" title="editar produto" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i></a>
-                                           <a href="<?= base_url() ?>usuarios/apagar/" title="Apagar cadastro" class="btn btn-sm btn-danger"> <i class="fa fa-trash" aria-hidden="true"></i>
-</a>
-                                       </td>
-                                   </tr>
-
-                               <?php endforeach ?>
-                           </tbody>
-
-                       </table>
+               <div class="col-lg-12 text-right">
+                   <a href="" title="Novo usuario" class="btn btn-success"><i class="fa fa-plus-square"></i></a>
                </div>
            </div>
 
+           <?php
+            // echo '<pre>';
+            // print_r($users);
+            // echo '</pre>';
+            ?>
+
+           <!-- Tabela -->
+           <div class="row">
+               <div class="col-lg-12">
+                   <table class="table table-bordered table-striped" id="datatable">
+                       <thead>
+                           <tr>
+                               <th>#</th>
+                               <th>Nome</th>
+                               <th>E-mail</th>
+                               <th>Ações</th>
+                               <!-- <th></th> -->
+                           </tr>
+                       </thead>
+                       <tbody>
+                           <?php foreach ($users as $user) : ?>
+                               <tr>
+                                   <td><?= $user->id ?></td>
+                                   <td><?= $user->username ?></td>
+                                   <td><?= $user->email ?></td>
+                                   <td class="text-right">
+                                       <a href="<?php base_url() ?>usuarios/editar/" title="editar produto" class="btn btn-sm btn-warning"><i class="fa fa-pencil"></i></a>
+                                       <a href="<?= base_url() ?>usuarios/apagar/" title="Apagar cadastro" class="btn btn-sm btn-danger"> <i class="fa fa-trash" aria-hidden="true"></i>
+                                       </a>
+                                   </td>
+                               </tr>
+
+                           <?php endforeach ?>
+                       </tbody>
+                   </table>
+               </div>
+           </div>
+           <!-- Fim tabela -->
+           
        </div>
        <!-- /.container-fluid -->
    </div>
