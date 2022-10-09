a7a2f9ee84e216bdd5db1a29f591b85b30b197b9
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-09 01:11:41 -0300

    Teste de envio Formulario e Verificaçoes1~

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -19,7 +19,7 @@ class Usuarios extends CI_Controller
             setar_msg('msgerro', 'Erro: Você precisa ser um administrador para acessar essa pagina', 'erro');
             redirect('home');
         }
-        
+
         $this->load->model('ion_auth_model');
     }
     public function index()
@@ -41,7 +41,6 @@ class Usuarios extends CI_Controller
     }
     public function add()
     {
-        // $dados['users'] = $this->ion_auth->users()->result(); // get all users
 
         // Titulo da aba no navegador
         $dados["titulo"] = "Novo usuario";
@@ -51,8 +50,32 @@ class Usuarios extends CI_Controller
         // Passa um conjunto de variaveis para as views
         $this->load->vars($dados);
 
-        $this->load->view('templates/header');
-        $this->load->view('pages/usuarios/novo');
-        $this->load->view('templates/footer');
+
+
+
+        // Validando os inputs
+        $this->form_validation->set_rules('nome_usuario', 'Nome', 'required');
+        $this->form_validation->set_rules('email_usuario', 'E-mail', 'required');
+        $this->form_validation->set_rules('senha_usuario', 'Senha', 'required');
+
+        if ($this->form_validation->run() == TRUE) {
+            echo '<pre>';
+            print_r($this->input->post());
+            echo '</pre>';
+
+            // $identity = $this->input->post('login');
+            // $password = $this->input->post('senha');
+            // $remember = TRUE; // remember the user
+            // if ($this->ion_auth->login($identity, $password, $remember)) {
+            //     redirect('home', 'refresh');
+            // } else {
+            //     setar_msg('msgerro', 'Os dados de acesso estão incorretos', 'erro');
+            //     redirect('login', 'refresh');
+            // }
+        } else {
+            $this->load->view('templates/header');
+            $this->load->view('pages/usuarios/novo');
+            $this->load->view('templates/footer');
+        }
     }
 }
--- application/views/pages/usuarios/novo.php
+++ application/views/pages/usuarios/novo.php
@@ -18,22 +18,27 @@
            </div>
            <!-- /.row -->
 
-           <!-- Tabela -->
+           <div class="row">
+               <div class="col-lg-12">
+                   <p><?= validation_errors() ?></p>
+               </div>
+           </div>
+           <!-- Formulario de novo usuario -->
            <div class="row">
                <div class="col-lg-12">
 
                    <form action="" method="post" class="" name="form_add">
                        <div class="form-group">
-                           <label for="exampleInputEmail1">Nome do usuarios</label>
-                           <input type="text" class="form-control" name="nome_usuario" id="exampleInputEmail1" placeholder="Nome do usuario">
+                           <label for="">Nome do usuarios</label>
+                           <input type="text" class="form-control" name="nome_usuario" id="" placeholder="Nome do usuario">
                        </div>
                        <div class="form-group">
-                           <label for="exampleInputPassword1">Email</label>
-                           <input type="email" class="form-control" name="email_usuario" id="exampleInputPassword1" placeholder="Eamil">
+                           <label for="">Email</label>
+                           <input type="email" class="form-control" name="email_usuario" id="" placeholder="Eamil">
                        </div>
                        <div class="form-group">
-                           <label for="exampleInputPassword1">Senha</label>
-                           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                           <label for="">Senha</label>
+                           <input type="password" class="form-control" name="senha_usuario" id="" placeholder="Senha">
                        </div>
 
                        <button form="form_add" type="submit" class="btn btn-success">Cadastrar</button>
@@ -41,11 +46,8 @@
                    </form>
                </div>
            </div>
-           <!-- Fim tabela -->
+           <!-- Fim do Form -->
 
        </div>
        <!-- /.container-fluid -->
-   </div>
-   <!-- /#page-wrapper -->
-   </div>
    </div>
